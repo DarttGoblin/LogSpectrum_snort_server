4ca53f122b0e70868f6c92efe2008b7efcef4d0a
DarttGoblin <yassine.bazgour@gmail.com>
2025-02-06 21:11:21 +0100

    Initial commit

---
+++ SnortLog.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+const port = 8011;
+const filePath = 'snort_afterPingAttack.alert.fast';
+
+// Enable CORS for all origins
+app.use(cors());
+
+// Route to fetch the file data
+app.get('/', (req, res) => {
+    fs.readFile(filePath, 'utf8', (err, snortLogFileData) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return res.status(500).json({ error: 'Error reading file' });
+        }
+
+        const snortLogFileDataLines = snortLogFileData.split('\n');
+        res.json(snortLogFileDataLines);
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+
+
+// const fs = require("fs");
+// const http = require("http");
+// const socketIo = require("socket.io");
+
+// const port = 8011;
+
+// const server = http.createServer();
+// const io = socketIo(server, {
+//     cors: {
+//         origin: "*",
+//         methods: ["GET", "POST"],
+//         allowedHeaders: ["Content-Type"],
+//         credentials: true
+//     }
+// });
+
+// const filePath = 'snort_afterPingAttack.alert.fast';
+
+// fs.watch(filePath, (eventType, filename) => {
+//     if (eventType === 'change') {
+//         fs.readFile(filePath, 'utf8', (err, snortLogFileData) => {
+//             if (err) {
+//                 console.error('Error reading file:', err);
+//                 return;
+//             }
+            
+//             const snortLogFileDataLines = snortLogFileData.split('\n');
+//             console.log('The file has been modified!');
+
+//             io.emit('fileChanged', snortLogFileDataLines);
+//         });
+//     }
+// });
+
+// io.on('connection', (socket) => {
+//     console.log('New client connected');
+
+//     fs.readFile(filePath, 'utf8', (err, snortLogFileData) => {
+//         if (err) {
+//             console.error('Error reading file:', err);
+//             return;
+//         }
+
+//         const snortLogFileDataLines = snortLogFileData.split('\n');
+//         socket.emit('fileChanged', snortLogFileDataLines);
+//     });
+
+//     socket.on('disconnect', () => {
+//         console.log('Client disconnected');
+//     });
+// });
+
+// server.listen(port, () => console.log("Listening on port " + port));
